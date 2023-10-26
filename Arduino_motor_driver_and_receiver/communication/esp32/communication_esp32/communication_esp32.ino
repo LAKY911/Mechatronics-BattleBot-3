84300b133271ed1cd71817a819c36079da05331a
nicolaiao <nicol@LAPTOP-RMTQOA0H.knowit.local>
2023-10-26 15:14:32 +0200

    added network mqtt

--- Arduino_motor_driver_and_receiver/communication/esp32/communication_esp32/communication_esp32.ino
+++ Arduino_motor_driver_and_receiver/communication/esp32/communication_esp32/communication_esp32.ino
@@ -1,27 +1,81 @@
-/*
-  #define RX2 16 // Reciever
-  #define TX2 17 // Transmitter
-void setup() {
-  // put your setup code here, to run once:
-  //Serial.begin(115200);
-  Serial2.begin(115200, SERIAL_8N1, RX2, TX2);
-}
-
-void loop() {
-    Serial.println("Message Received: ");
-    Serial.println(Serial2.readString());
-}
-*/
 #include <HardwareSerial.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-void setup() {
-  Serial2.begin(115200); // Initialize serial communication on UART2 at 115200 baud
-  Serial.begin(115200);  // Initialize the main serial communication for debugging
+
+// Update these with values suitable for your network.
+const char* ssid = "Nicolai's iPhone";
+const char* password = "balle.stein";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+#define mqtt_port 8884
+#define MQTT_USER "test"
+#define MQTT_PASSWORD "test"
+#define MQTT_SERIAL_PUBLISH_CH "/ic/esp32/serialdata/uno/"
+
+WiFiClient wifiClient;
+
+PubSubClient client(wifiClient);
+
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  if (Serial2.available()) {
-    String receivedMessage = Serial2.readStringUntil('\n'); // Read the incoming string on UART2
-    Serial.println("Received: " + receivedMessage); // Print the received string
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    client.publish("outTopic", "hello world");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(),MQTT_USER,MQTT_PASSWORD)) {
+      Serial.println("connected");
+      //Once connected, publish an announcement...
+      client.publish("/ic/presence/esp32/", "hello world");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
 }
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(500);// Set time out for 
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  reconnect();
+}
+
+void publishSerialData(char *serialData){
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.publish(MQTT_SERIAL_PUBLISH_CH, serialData);
+}
+void loop() {
+   client.loop();
+   if (Serial.available() > 0) {
+     char bfr[501];
+     memset(bfr,0, 501);
+     Serial.readBytesUntil( '\n',bfr,500);
+     publishSerialData(bfr);
+   }
+ }
