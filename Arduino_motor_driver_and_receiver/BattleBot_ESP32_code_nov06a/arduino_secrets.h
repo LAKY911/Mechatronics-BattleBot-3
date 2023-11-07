866f94fa1fd714c90d5634c9928795ecfd8a98b6
Nicolai Olsen <nicolaiolsen98@gmail.com>
2023-11-07 12:14:07 +0100

    aiusefh

--- Arduino_motor_driver_and_receiver/BattleBot_ESP32_code_nov06a/arduino_secrets.h
+++ Arduino_motor_driver_and_receiver/BattleBot_ESP32_code_nov06a/arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_SSID ""
-#define SECRET_OPTIONAL_PASS ""
-#define SECRET_DEVICE_KEY ""
+#define SECRET_SSID "NTB-DELL-G5 6364"
+#define SECRET_OPTIONAL_PASS "deltarobot"
+#define SECRET_DEVICE_KEY "295e8478-c206-416b-ac5d-72b7d60610c5"
